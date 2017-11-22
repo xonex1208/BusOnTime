001d4b5c35ed115a66e353ee928fb999d7ecc7d2
xonex1208 <xonex1208@users.noreply.github.com>
2017-11-22 10:32:28 -0600

    rama cesar

---
+++ app/src/main/java/bus/proyecto/busontime/Activities/Paypal.java
@@ -0,0 +1,13 @@
+package bus.proyecto.busontime.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Paypal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paypal);
+    }
+}
