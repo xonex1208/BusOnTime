2f04d6b0f4a774344fecaedbc2908fa8bdeea459
xonex1208 <xonex1208@users.noreply.github.com>
2017-11-23 15:02:52 -0600

    rama cesar

--- app/src/main/java/bus/proyecto/busontime/Activities/Registro.java
+++ app/src/main/java/bus/proyecto/busontime/Activities/Registro.java
@@ -39,7 +39,7 @@ import bus.proyecto.busontime.operaciones.Pasajero;
 public class Registro extends AppCompatActivity {
     private String cad;
     private Spinner sp;
-    private AppCompatActivity contexto=this;
+    private AppCompatActivity contexto = this;
     Conectar conectar;
     CallbackManager callbackManager;
     LoginButton lognbtn;
@@ -56,35 +56,35 @@ public class Registro extends AppCompatActivity {
     private TextInputLayout til_apMR;
     private TextInputLayout til_passR;
     private TextInputLayout til_repetirpassR;
-    String nombreS ,apPS,apMS,passS,repetirpassS;
-    private boolean mail2=false,nombre2=false,apeP=false,apeM=false,pass=false,passR=false;
+    String nombreS, apPS, apMS, passS, repetirpassS;
+    private boolean mail2 = false, nombre2 = false, apeP = false, apeM = false, pass = false, passR = false;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
-        conectar=new Conectar(this,"http://busontime.herokuapp.com");
-        lognbtn = (LoginButton)findViewById(R.id.loginfb);
-        lognbtn.setReadPermissions("email","public_profile");
+        conectar = new Conectar(this, "http://busontime.herokuapp.com");
+        lognbtn = (LoginButton) findViewById(R.id.loginfb);
+        lognbtn.setReadPermissions("email", "public_profile");
         callbackManager = (CallbackManager.Factory.create());
         lognbtn.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                String userid=loginResult.getAccessToken().getUserId();
-                GraphRequest graphrqst= GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                String userid = loginResult.getAccessToken().getUserId();
+                GraphRequest graphrqst = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
-                        displayUserInfor(object );
+                        displayUserInfor(object);
                     }
                 });
                 Bundle parameters = new Bundle();
-                parameters.putString("fields","first_name,last_name,email,id");
+                parameters.putString("fields", "first_name,last_name,email,id");
                 graphrqst.setParameters(parameters);
                 graphrqst.executeAsync();
             }
 
             @Override
             public void onCancel() {
-                Toast.makeText(Registro.this,"Ha cancelado el registro",Toast.LENGTH_LONG).show();
+                Toast.makeText(Registro.this, "Ha cancelado el registro", Toast.LENGTH_LONG).show();
             }
 
             @Override
@@ -92,84 +92,94 @@ public class Registro extends AppCompatActivity {
 
             }
         });
-        texNombre=(EditText)findViewById(R.id.regnombre);
-        texApaterno=(EditText)findViewById(R.id.regapaterno);
-        texMaterno=(EditText)findViewById(R.id.regamaterno);
-        texEmail=(EditText)findViewById(R.id.regemail);
-        texPass=(EditText)findViewById(R.id.regcontrasena);
-        texRepPass=(EditText)findViewById(R.id.regrepetir_contrasena);
-        bRegistrar=(Button)findViewById(R.id.btn_registro);
-        til_mail=(TextInputLayout)findViewById(R.id.TIL_emailR);
+        texNombre = (EditText) findViewById(R.id.regnombre);
+        texApaterno = (EditText) findViewById(R.id.regapaterno);
+        texMaterno = (EditText) findViewById(R.id.regamaterno);
+        texEmail = (EditText) findViewById(R.id.regemail);
+        texPass = (EditText) findViewById(R.id.regcontrasena);
+        texRepPass = (EditText) findViewById(R.id.regrepetir_contrasena);
+        bRegistrar = (Button) findViewById(R.id.btn_registro);
+        til_mail = (TextInputLayout) findViewById(R.id.TIL_emailR);
 
-        til_nombreR=(TextInputLayout)findViewById(R.id.TIL_nombreR);
-        til_apPR=(TextInputLayout)findViewById(R.id.TIL_apPR);
-        til_apMR=(TextInputLayout)findViewById(R.id.TIL_apMR);
-        til_passR=(TextInputLayout)findViewById(R.id.TIL_contraR);
-        til_repetirpassR=(TextInputLayout)findViewById(R.id.TIL_repetirContraR);
+        til_nombreR = (TextInputLayout) findViewById(R.id.TIL_nombreR);
+        til_apPR = (TextInputLayout) findViewById(R.id.TIL_apPR);
+        til_apMR = (TextInputLayout) findViewById(R.id.TIL_apMR);
+        til_passR = (TextInputLayout) findViewById(R.id.TIL_contraR);
+        til_repetirpassR = (TextInputLayout) findViewById(R.id.TIL_repetirContraR);
         bRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String email= texEmail.getText().toString();
-                if (Patterns.EMAIL_ADDRESS.matcher(email).matches()==false){
+                String email = texEmail.getText().toString();
+                if (Patterns.EMAIL_ADDRESS.matcher(email).matches() == false) {
                     til_mail.setError("Ingrese un correo valido");
-                    mail2=false;
-                }else{
-                til_mail.setError(null);
-                Pasajero pasajero=new Pasajero();
-                pasajero.setNombre(texNombre.getText()+"");
-                pasajero.setApaterno(texApaterno.getText()+"");
-                pasajero.setAmaterno(texMaterno.getText()+"");
-                pasajero.setEmail(texEmail.getText()+"");
-                pasajero.setContraseña(texPass.getText()+"");
-                conectar.registrarPasajero(pasajero,onError,respuesta);
-                    mail2=true;
+                    mail2 = false;
+                } else {
+                    til_mail.setError(null);
+
+                    mail2 = true;
                 }
-                nombreS= texNombre.getText().toString();
-                apPS=texApaterno.getText().toString();
-                apMS=texMaterno.getText().toString();
-                passS=texPass.getText().toString();
-                repetirpassS=texRepPass.getText().toString();
-                String mensaje="Campo obligatorio";
-                if ((nombreS.isEmpty()&&nombreS!=null)){
+                nombreS = texNombre.getText().toString();
+                apPS = texApaterno.getText().toString();
+                apMS = texMaterno.getText().toString();
+                passS = texPass.getText().toString();
+                repetirpassS = texRepPass.getText().toString();
+                String mensaje = "Campo obligatorio";
+                if ((nombreS.isEmpty() && nombreS != null)) {
                     til_nombreR.setError(mensaje);
-                    nombre2=false;
-                }else{
+                    nombre2 = false;
+                } else {
                     til_nombreR.setError(null);
-                    nombre2=true;
+                    nombre2 = true;
 
                 }
-                if((apPS.isEmpty()&&apPS!=null)){
+                if ((apPS.isEmpty() && apPS != null)) {
                     til_apPR.setError(mensaje);
-                    apeP=false;
-                }else {
+                    apeP = false;
+                } else {
                     til_apPR.setError(null);
-                    apeP=true;
+                    apeP = true;
                 }
 
-                if((apMS.isEmpty()&&apMS!=null)){
+                if ((apMS.isEmpty() && apMS != null)) {
                     til_apMR.setError(mensaje);
-                    apeM=false;
-                }else {
+                    apeM = false;
+                } else {
                     til_apMR.setError(null);
-                    apeM=true;
+                    apeM = true;
                 }
 
-                if((passS.isEmpty()&& passS!=null)){
+                if ((passS.isEmpty() && passS != null)) {
                     til_passR.setError(mensaje);
-                    pass=false;
-                }else {
+                    pass = false;
+                } else {
                     til_passR.setError(null);
-                    pass=true;
+                    pass = true;
 
                 }
 
-                if ((repetirpassS.isEmpty()&& repetirpassS!=null)){
+                if ((repetirpassS.isEmpty() && repetirpassS != null)) {
                     til_repetirpassR.setError(mensaje);
-                    passR=false;
-                }else{
-                    til_repetirpassR.setError(mensaje);
-                    passR=true;
+                    passR = false;
+                } else {
+                    if (!repetirpassS.equals(passS)) {
+                        til_repetirpassR.setError("La contraseña es diferente");
+                        passR = false;
+                    } else {
+                        til_repetirpassR.setError(null);
+                        passR = true;
+                    }
+
+                }
+
+                if(nombre2&&mail2&&apeP&&apeM&&pass&&passR){
+                    Pasajero pasajero = new Pasajero();
+                    pasajero.setNombre(texNombre.getText() + "");
+                    pasajero.setApaterno(texApaterno.getText() + "");
+                    pasajero.setAmaterno(texMaterno.getText() + "");
+                    pasajero.setEmail(texEmail.getText() + "");
+                    pasajero.setContraseña(texPass.getText() + "");
+                    conectar.registrarPasajero(pasajero, onError, respuesta);
                 }
 
 
@@ -178,15 +188,14 @@ public class Registro extends AppCompatActivity {
     }
 
 
-
-    public void displayUserInfor(JSONObject object){
-        String first_name,last_name,email,id;
-        first_name="";
-        last_name="";
-        email="";
-        id="";
+    public void displayUserInfor(JSONObject object) {
+        String first_name, last_name, email, id;
+        first_name = "";
+        last_name = "";
+        email = "";
+        id = "";
         try {
-            first_name=object.getString("first_name");
+            first_name = object.getString("first_name");
             last_name = object.getString("last_name");
             email = object.getString("email");
             id = object.getString("id");
@@ -194,13 +203,13 @@ public class Registro extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        TextView tv_name,tv_email,tv_id;
-        tv_name=(TextView)findViewById(R.id.TV_name);
-        tv_email=(TextView)findViewById(R.id.TV_email);
-        tv_id=(TextView)findViewById(R.id.TV_id);
-        tv_name.setText(first_name+" "+last_name);
-        tv_email.setText("Email: "+email);
-        tv_id.setText("ID: "+id);
+        TextView tv_name, tv_email, tv_id;
+        tv_name = (TextView) findViewById(R.id.TV_name);
+        tv_email = (TextView) findViewById(R.id.TV_email);
+        tv_id = (TextView) findViewById(R.id.TV_id);
+        tv_name.setText(first_name + " " + last_name);
+        tv_email.setText("Email: " + email);
+        tv_id.setText("ID: " + id);
     }
 
     @Override
@@ -209,14 +218,14 @@ public class Registro extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
-    private Response.ErrorListener onError=new Response.ErrorListener() {
+    private Response.ErrorListener onError = new Response.ErrorListener() {
         @Override
         public void onErrorResponse(VolleyError error) {
             Toast.makeText(contexto, "Error al registrarse", Toast.LENGTH_LONG).show();
         }
     };
 
-    private Response.Listener<String> respuesta=new Response.Listener<String>() {
+    private Response.Listener<String> respuesta = new Response.Listener<String>() {
         @Override
         public void onResponse(String response) {
             Toast.makeText(contexto, "Usuario registrado correctamente", Toast.LENGTH_LONG).show();
