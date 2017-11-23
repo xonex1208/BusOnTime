6cffa89c08daf89e623bcad03944eed25b710ef7
xonex1208 <xonex1208@users.noreply.github.com>
2017-11-22 20:25:41 -0600

    rama cesar

--- app/src/main/java/bus/proyecto/busontime/Activities/Paypal.java
+++ app/src/main/java/bus/proyecto/busontime/Activities/Paypal.java
@@ -3,6 +3,8 @@ package bus.proyecto.busontime.Activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import bus.proyecto.busontime.R;
+
 public class Paypal extends AppCompatActivity {
 
     @Override
